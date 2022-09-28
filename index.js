1c3e0e2ee6008ebc0899fd29bff073ebe5d6a669
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-09-28 13:06:50 +0300

    feat: add search functionality to the phonebook

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { request, response } = require("express");
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const phoneBook = [
   {
@@ -23,3 +25,38 @@ const phoneBook = [
     number: "39-23-6423122",
   },
 ];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello,  Welcome to my phonebook😄 </h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phoneBook);
+});
+
+app.get("/info", (request, response) => {
+  response.send(`<h1>Phone book has info for ${phoneBook.length} people </h1>
+  ${new Date()}
+  `);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+
+  const entry = phoneBook.find((entry) => entry.id === id);
+  console.log(entry);
+
+  if (entry) {
+    response.json(entry);
+  } else {
+    response.status(404).end("<h1>Entry not available in our phonebook</h1>");
+  }
+});
+
+app.get();
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`server is runnng on port ${PORT}`);
+});
